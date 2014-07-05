97c5f8c13a880cd0bc67308dc65fca824761f0d6
Carlos Torchia <carlos@trendlier.com>
2014-07-05 02:04:10 +0000

    paginate by page height instead of word count

--- sites/all/themes/platform/field_fun.php
+++ sites/all/themes/platform/field_fun.php
@@ -1,18 +1,5 @@
 <?php
 
-require_once('word_count_paginate.php');
-
-function paginate_field_text(&$node, $field_name, $page_size)
-{
-    $field_text_items = field_get_items('node', $node, $field_name);
-    $field_text = '';
-    foreach ($field_text_items as &$field_text_item)
-    {
-        $field_text .= paginate_text($field_text_item['value'], $page_size);
-    }
-    return $field_text;
-}
-
 function get_field_text($node, $field_name)
 {
     $field_text_items = field_get_items('node', $node, $field_name);
--- sites/all/themes/platform/page--node--news_post.tpl.php
+++ sites/all/themes/platform/page--node--news_post.tpl.php
@@ -3,9 +3,24 @@
 require_once('field_fun.php');
 
 /**
+ * Determine page dimensions
+ */
+if (array_key_exists('page', $_GET))
+{
+    $page_number = $_GET['page'];
+    $page_offset = ($page_number > 1 ? 19: 0) + ($page_number - 1) * 323;
+    $page_height = ($page_number == 1 ? 342 : 323);
+}
+else
+{
+    $page_offset = 0;
+    $page_height = 342;
+}
+
+/**
  * Get field text
  */
-$field_text = paginate_field_text($node, 'field_text', 30);
+$field_text = get_field_text($node, 'field_text');
 
 /**
  * Get field subtitle
@@ -29,19 +44,33 @@ $product_image_url = get_field_image_url($product_node, 'field_image');
         <title><?php print $node->title; ?></title>
         <meta name="description" content="<?php print $field_subtitle; ?>">
         <style type="text/css">
-            h1 {font-family: RionaSans-Bold;line-height: 16px;font-size: 12pt;}
-            p {font-family: RionaSans-Regular;font-size: 9.5pt;}
+            h1 {font-family: RionaSans-Bold;font-size: 18px;line-height: 19px;}
+            p {font-family: RionaSans-Regular;font-size: 12px;}
+            .page_container {
+                position: relative;
+                overflow: hidden;
+                height: <?php print $page_height; ?>px;
+                width: 296px;
+                margin: auto;
+                line-height: 19px;
+            }
+            .page_offset_container {
+                position: relative;
+                top: -<?php print $page_offset; ?>px;
+            }
         </style>
     </head>
     <body>
-        <div style="margin: auto; width: 296px;">
-        <div style="height:160px;overflow:hidden">
-            <img width="296px" src="<?php print $product_image_url; ?>" />
-        </div>
-        <div style="width: 284px">
-            <h1><?php print $field_subtitle; ?></h1>
-            <p><?php print $field_text; ?></p>
-        </div>
+        <div class="page_container">
+            <div class="page_offset_container">
+                <div style="height:148px;overflow:hidden">
+                    <img width="296px" src="<?php print $product_image_url; ?>" />
+                </div>
+                <div style="width: 284px">
+                    <h1><?php print $field_subtitle; ?></h1>
+                    <p><?php print $field_text; ?></p>
+                </div>
+            </div>
         </div>
     </body>
 </html>
